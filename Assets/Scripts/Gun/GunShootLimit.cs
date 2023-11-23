e4ef6f13f2b338eed37c2c6fc04945dd80ea7249
Gustavo Emanuel F. Araújo <gustavogefa@hotmail.com>
2023-11-22 21:34:05 -0300

    M26.5 - Add UI to the gun

--- Assets/Scripts/Gun/GunShootLimit.cs
+++ Assets/Scripts/Gun/GunShootLimit.cs
@@ -1,14 +1,21 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimit : GunBase
 {
+  public List<UIGunUpdater> uIGunUpdaters;
   public float maxShoot = 5f;
   public float timeToReload = 1f;
 
   private float _currentShoots;
   private bool _reloading = false;
+
+  private void Awake()
+  {
+    GetAllUIs();
+  }
 
   protected override IEnumerator ShootCorountine()
   {
@@ -19,6 +26,7 @@ public class GunShootLimit : GunBase
       Shoot();
       _currentShoots++;
       CheckReload();
+      UpdateUI(maxShoot, maxShoot - _currentShoots);
       yield return new WaitForSeconds(timeBetweenShoots);
     }
   }
@@ -43,10 +51,22 @@ public class GunShootLimit : GunBase
     float time = 0;
     while (time < timeToReload)
     {
+      UpdateUI(timeToReload, time);
       time += Time.deltaTime;
       yield return new WaitForEndOfFrame();
     }
+    UpdateUI(1, 1);
     _currentShoots = 0;
     _reloading = false;
+  }
+
+  private void UpdateUI(float max, float current)
+  {
+    uIGunUpdaters.ForEach(i => i.UpdateValue(max, current));
+  }
+
+  private void GetAllUIs()
+  {
+    uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
   }
 }
---
+++ Assets/Scripts/Gun/UIGunUpdater.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System;
+
+public class UIGunUpdater : MonoBehaviour
+{
+    public Image uiImage;
+
+    [Header("Animation")]
+    public float baseAnimationDuration = 0.05f;
+    public float distanceAnimationMultiplier = 0.5f;
+    public Ease ease = Ease.InOutSine;
+
+    private void OnValidate()
+    {
+        if (uiImage == null)
+        {
+            uiImage = GetComponent<Image>();
+        }
+    }
+
+    public void UpdateValue(float f)
+    {
+        float duration = Math.Abs(uiImage.fillAmount - f) * distanceAnimationMultiplier + baseAnimationDuration;
+        uiImage.DOKill();
+        uiImage.DOFillAmount(f, duration).SetEase(ease);
+    }
+
+    public void UpdateValue(float max, float current)
+    {
+        UpdateValue(current / max);
+    }
+}
