4209189025bd19f312f0b1a809774efbf51f7ddf
Gustavo Emanuel F. Araújo <gustavogefa@hotmail.com>
2024-01-09 20:41:27 -0300

    M32.4 - Add coins dropping from tree

--- Assets/Scripts/Items/DestructableItemBase.cs
+++ Assets/Scripts/Items/DestructableItemBase.cs
@@ -10,6 +10,13 @@ public class DestructableItemBase : MonoBehaviour
     public float shareDuration = .1f;
     public int shakeForce = 1;
 
+    public int maxNumberOfCoinsToDrop = 10;
+    private int _numberOfCoinsDropped = 0;
+    public float dropChance = .5f;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
+
+
     private void OnValidate()
     {
         if (healthBase == null)
@@ -30,5 +37,28 @@ public class DestructableItemBase : MonoBehaviour
         transform.DOKill();
         transform.localScale = _originalScale;
         transform.DOShakeScale(shareDuration, Vector3.up * .2f, shakeForce);
+        if (Random.Range(0f, 1f) < dropChance)
+        {
+            DropCoins();
+        }
+    }
+
+    private void DropCoins()
+    {
+        if (_numberOfCoinsDropped >= maxNumberOfCoinsToDrop)
+        {
+            return;
+        }
+
+        _numberOfCoinsDropped++;
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0f, 1f).SetEase(Ease.OutBack).From();
+        Vector3 initialVelocity = Vector3.forward * Random.Range(-.1f, .1f) + Vector3.right * Random.Range(-.1f, .1f);
+        initialVelocity /= initialVelocity.magnitude;
+        initialVelocity *= 2f;
+        initialVelocity += Vector3.up * 10f;
+        i.GetComponent<Rigidbody>().velocity = initialVelocity;
+
     }
 }
